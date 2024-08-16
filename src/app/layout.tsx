5c209837080ead2312cdc3d68cfe77edf21ab85e
flowcore-platform <gitops@flowcore.io>
2024-08-16 20:53:03 +0100

    feat: added blog title to menu

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -53,12 +53,12 @@ export default async function RootLayout({
           <ThemeProvider attribute={"class"} defaultTheme={"light"} disableTransitionOnChange enableSystem>
             <TRPCReactProvider cookies={cookies().toString()}>
               <div className="flex flex-col min-h-screen">
-                <header className="flex items-center h-16 px-4 border-b shrink-0 md:px-6">
-                  <Link href="#" className="flex items-center gap-2 text-lg font-semibold md:text-base" prefetch={false}>
-                    <MountainIcon className="w-6 h-6" />
-                    <span className="sr-only">Acme Inc</span>
-                  </Link>
-                  <nav className="hidden gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6 ml-auto">
+              <header className="flex items-center h-16 px-4 border-b shrink-0 md:px-6">
+                <Link href="#" className="flex items-center gap-2 text-lg font-semibold md:text-base" prefetch={false}>
+                  <MountainIcon className="w-6 h-6" />
+                  <span className="text-lg font-semibold">The Caffeinated Codebase</span>
+                </Link>
+                <nav className="hidden gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6 ml-auto">
                     <Link href="#" className="font-bold" prefetch={false}>
                       Home
                     </Link>
